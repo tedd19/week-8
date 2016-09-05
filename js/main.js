1012422541332ad6cbfb6639ca9acf4e78af9b36
tedd19 <uconn0019@gmail.com>
2016-09-05 16:38:57 -0700

    2nd commit

--- js/main.js
+++ js/main.js
@@ -2,19 +2,102 @@ var zodiac = [
 	{
 		sign: "aquarius",
 		fortune: "Aquarius can be considered as the sign of originality and change because they have that power to make some changes in areas. Aquarius-born people are included in the air element because others won’t be able to predict them. They are too erratic, but their mind will always be filled with some concerns, like the mankind’s future and advancing civilization. When it comes to the advanced thinking, Aquarius is number one in this element.",
-		image: "img/aquarius.png"
+		image: "img/aquarius.png",
+		info: "Air is something that can be considered as a mysterious thing because it is the element that cannot be seen and touched but air is an essential element for every creature on the earth. Air cannot be predicted and it is not a certain thing, which is why people with air element are read as independent people because they love freedom. They may have inadequate emotional depth, but they are known as mental-oriented people who will always think in very advanced. People with air signs will always be seen friendly because they like communicating with others and building good relationships. Air people are not only friendly, but they are also analytical, communicative, intellectual, but they are actually thinkers. For deep conversations, it is the best to talk to them because they like philosophies. What these people enjoy a lot are good books and social gatherings. They will gladly give some advice to people whom they talk with, but sometimes they can also be superficial.",
+		name: "Air Sign"
 	},
+	{
+		sign: "aries",
+		fortune: "As mentioned previously, the fire people have temperamental trait and Aries does have this characteristic along with their dynamic side. They are always filled with enthusiasm just like a child that is always excited about everything. They have adventurous soul and that is why they would like to try something new and have interest in everything as long as they can be active. This sign is the purest form in fire element.",
+		image: "img/aries.png",
+		info: "Fire may seem harmful because it can burn, but fire is good for our life since it is the element that can be used to make us stay warm and that can even be utilized to cook foods. Fire cannot be predicted and it is no doubt that people with fire element can be temperamental; but it is could be beneficial if their temperaments can be managed in a right way. They are passionate people and they may be impulsive at times. People with fire signs are temperamental and even if they may be in anger quickly, it is easy and quick as well for them to forgive others who make them angry. These people would like to adventure because they have enormous energy; that is the reason why they are known to be dynamic. Fire people will always be considered as an inspiration’s source for other people. They have a strong physique and passion in whatever they do. They tend to be more ready for actions because they are creative and intelligent. The other characteristics that are identified are that these people are idealistic and self-aware.",
+		name: "Fire Sign"
+	},
+	{
+		sign: "cancer",
+		fortune: "Cancer-born people are helpful and nurturing as they care about people around them the most. They are known to be genuine. However, Cancer has an aggressive side; this side will definitely appear when they need to defend themselves or people they love. Having friends with Cancer sign will be beneficial for us because they will always concern with us.", 
+		image: "img/cancer.png",
+		info: "Water may be mysterious and it may also be dark, but they are cool and refreshing. Water people are easier to cope with even if they are the most emotional one if compared to the other elements. They have a high humanity sense and that leads them to understand others better than other elements. Water people will know how to summarize the others’ needs and motives. The best qualities that you may find from water people are helpful, caring, and empathetic even though they are a bit annoying when they are too dramatic and over-sensitive since they are easily hurt. What they enjoy the most are intimacy and love profound conversations. Besides, they can memorize everything well because their memory is great. It is alright for them to criticize themselves because they are so open. They are also loyal proven by how they will always be there for their loved ones who need supports.",
+		name: "Water Sign"
+	},
+	{
+		sign: "capicorn",
+		fortune: "Capricorn is mentioned to be the purest form of earth because they have powerful status and materialistic possessions symbols. They are unique because they are conservative; they are also too deep and mysterious. The seriousness when it comes to work cannot be avoided and that is why they do not have an easy time to have some pleasure as they focus on their work all the time.", 
+		image: "img/capicorn.png",
+		info: "Earth is more visible and touchable and we can even smell it. People with earth element are known to have a good stability and they are always composed and solid. The things that are good news for people around them are that Earth people won’t easily change and they are dependable. Regular life is identical with them because they find no reasons to dig something new when everything around them is alright. People with earth signs are really down to earth so they will give others good influence in life. Although they are sometimes emotional, they are known to have realistic quality and have conservative style. What they enjoy the most are material and luxury goods. Being friends with these people are great because these people will always be our side to go through ups and downs together since they are loyal. Besides, their stability and loyalty are also the best qualities.",
+		name: "Earth Sign"
+	},
+	{
+		sign: "gemini",
+		fortune: "Indecisiveness is one word that may be able to describe this sign in the air element. Gemini-born people will never be satisfied with what they get because they always look for something new as if they feel incomplete every time. They may keep searching until they find a thing or person that is right and able to complete them. Communication is one thing that is really considered important by the Gemini and that is the reason why they won’t hesitate to know everything and everybody they meet.",
+		image: "img/gemini.png",
+		info: "Air is something that can be considered as a mysterious thing because it is the element that cannot be seen and touched but air is an essential element for every creature on the earth. Air cannot be predicted and it is not a certain thing, which is why people with air element are read as independent people because they love freedom. They may have inadequate emotional depth, but they are known as mental-oriented people who will always think in very advanced. People with air signs will always be seen friendly because they like communicating with others and building good relationships. Air people are not only friendly, but they are also analytical, communicative, intellectual, but they are actually thinkers. For deep conversations, it is the best to talk to them because they like philosophies. What these people enjoy a lot are good books and social gatherings. They will gladly give some advice to people whom they talk with, but sometimes they can also be superficial.",
+		name: "Air Sign"
+	},
+	{
+		sign: "leo",
+		fortune: "As it is in the middle in fire element, the one that is loved the most by Leo-born people is being a center of attention and that is the perfect spot for them to feel like they are the best and their performance will even the maximum one. It is good to have friends or relatives with Leo sign because they are reliable and stable; they are even able to keep others under control due to their radiance and stability.",
+		image: "img/leo.png",
+		info: "Fire may seem harmful because it can burn, but fire is good for our life since it is the element that can be used to make us stay warm and that can even be utilized to cook foods. Fire cannot be predicted and it is no doubt that people with fire element can be temperamental; but it is could be beneficial if their temperaments can be managed in a right way. They are passionate people and they may be impulsive at times. People with fire signs are temperamental and even if they may be in anger quickly, it is easy and quick as well for them to forgive others who make them angry. These people would like to adventure because they have enormous energy; that is the reason why they are known to be dynamic. Fire people will always be considered as an inspiration’s source for other people. They have a strong physique and passion in whatever they do. They tend to be more ready for actions because they are creative and intelligent. The other characteristics that are identified are that these people are idealistic and self-aware.",
+		name: "Fire Sign"
+	},
+	{
+		sign: "libra",
+		fortune: "Air element represents Libra sign as something that does not move but there is slow circulation just like the air our body needs in breathing. There are three essential things brought by this sign, balance, life and harmony. It can be identified easily that in Libra’s relationships, there is always balance and this thing is actually important in every relationship. Just like air, Libra is important for the people around them and they are also known to be peaceful.",
+		image: "img/libra.png",
+		info: "Air is something that can be considered as a mysterious thing because it is the element that cannot be seen and touched but air is an essential element for every creature on the earth. Air cannot be predicted and it is not a certain thing, which is why people with air element are read as independent people because they love freedom. They may have inadequate emotional depth, but they are known as mental-oriented people who will always think in very advanced. People with air signs will always be seen friendly because they like communicating with others and building good relationships. Air people are not only friendly, but they are also analytical, communicative, intellectual, but they are actually thinkers. For deep conversations, it is the best to talk to them because they like philosophies. What these people enjoy a lot are good books and social gatherings. They will gladly give some advice to people whom they talk with, but sometimes they can also be superficial.",
+		name: "Air Sign"
+	},
+	{
+		sign: "pisces",
+		fortune: "Pisces-born people have no difficulties when it comes to adaptation because they have a higher flexibility so they are able to get along with others very well and quick. They are indeed mystical, but they are encouraging and attractive in their own way. Pisces-born people love spiritual things because its sign itself is the most spiritual when you compare to the other zodiac signs.", 
+		image: "img/pisces.png",
+		info: "Water may be mysterious and it may also be dark, but they are cool and refreshing. Water people are easier to cope with even if they are the most emotional one if compared to the other elements. They have a high humanity sense and that leads them to understand others better than other elements. Water people will know how to summarize the others’ needs and motives. The best qualities that you may find from water people are helpful, caring, and empathetic even though they are a bit annoying when they are too dramatic and over-sensitive since they are easily hurt. What they enjoy the most are intimacy and love profound conversations. Besides, they can memorize everything well because their memory is great. It is alright for them to criticize themselves because they are so open. They are also loyal proven by how they will always be there for their loved ones who need supports.",
+		name: "Water Sign"
+	},
+	{
+		sign: "sagittarius",
+		fortune: "Sagittarius is considered as the one that is out of reach, but their creativity and idealistic are their strengths. Among the three signs in fire element, Sagittarius is the gentlest one, just like the fire that goes out slowly. As they are idealistic, so the ideals they have are high; they usually use philosophy as well.",
+		image: "img/sagittariur.png",
+		info: "Fire may seem harmful because it can burn, but fire is good for our life since it is the element that can be used to make us stay warm and that can even be utilized to cook foods. Fire cannot be predicted and it is no doubt that people with fire element can be temperamental; but it is could be beneficial if their temperaments can be managed in a right way. They are passionate people and they may be impulsive at times. People with fire signs are temperamental and even if they may be in anger quickly, it is easy and quick as well for them to forgive others who make them angry. These people would like to adventure because they have enormous energy; that is the reason why they are known to be dynamic. Fire people will always be considered as an inspiration’s source for other people. They have a strong physique and passion in whatever they do. They tend to be more ready for actions because they are creative and intelligent. The other characteristics that are identified are that these people are idealistic and self-aware.",
+		name: "Fire Sign"
+	},
+	{
+		sign: "scorpio",
+		fortune: "Scorpio-born people are intense and they are always full of energy; it is right to compare the trait to a raging river. It seems difficult for them to have self control because when the full energy is not managed well, it won’t be something effective. They are also protective over the ones they love the most.", 
+		image: "img/scorpio.png",
+		info: "Water may be mysterious and it may also be dark, but they are cool and refreshing. Water people are easier to cope with even if they are the most emotional one if compared to the other elements. They have a high humanity sense and that leads them to understand others better than other elements. Water people will know how to summarize the others’ needs and motives. The best qualities that you may find from water people are helpful, caring, and empathetic even though they are a bit annoying when they are too dramatic and over-sensitive since they are easily hurt. What they enjoy the most are intimacy and love profound conversations. Besides, they can memorize everything well because their memory is great. It is alright for them to criticize themselves because they are so open. They are also loyal proven by how they will always be there for their loved ones who need supports.",
+		name: "Water Sign"
+	},
+	{
+		sign: "taurus",
+		fortune: "Among the three, Taurus is the strongest and has the highest stability. Taurus-born people are wise, nurturing, and reliable because they would like to help people around them to build a solid foundation in their life just like Taurus people do to themselves. They are clingy to things and people surrounding them, but as they are too stable, they are also known to have stubbornness.", 
+		image: "img/taurus.png",
+		info: "Earth is more visible and touchable and we can even smell it. People with earth element are known to have a good stability and they are always composed and solid. The things that are good news for people around them are that Earth people won’t easily change and they are dependable. Regular life is identical with them because they find no reasons to dig something new when everything around them is alright. People with earth signs are really down to earth so they will give others good influence in life. Although they are sometimes emotional, they are known to have realistic quality and have conservative style. What they enjoy the most are material and luxury goods. Being friends with these people are great because these people will always be our side to go through ups and downs together since they are loyal. Besides, their stability and loyalty are also the best qualities.",
+		name: "Earth Sign"
+	},
+	{
+		sign: "virgo",
+		fortune: "Since the middle of earth representing Virgo sign, it can be read that Virgo-born people are also identical with indecisiveness as they are stuck in the middle. However, they are always stable and helpful so they are reliable enough. Also, their flexibility is good, added with quick observation and analytical skill that make them more adorable in their own way.", 
+		image: "img/virgo.png",
+		info: "Earth is more visible and touchable and we can even smell it. People with earth element are known to have a good stability and they are always composed and solid. The things that are good news for people around them are that Earth people won’t easily change and they are dependable. Regular life is identical with them because they find no reasons to dig something new when everything around them is alright. People with earth signs are really down to earth so they will give others good influence in life. Although they are sometimes emotional, they are known to have realistic quality and have conservative style. What they enjoy the most are material and luxury goods. Being friends with these people are great because these people will always be our side to go through ups and downs together since they are loyal. Besides, their stability and loyalty are also the best qualities.",
+		name: "Earth Sign"
+	},
+
 ]
 
 function horoscope() {
-		var sign = document.getElementById("sign").value.toLowerCase
+		var sign = document.getElementById("sign").value.toLowerCase()
 
 		for(i = 0; i < zodiac.length; i++) {
 				if(sign === zodiac[i].sign) {
 						document.getElementById("yourSign").innerHTML = zodiac[i].sign
 						document.getElementById("icon").src = zodiac[i].image
-						document("yourHoroscope").innerHTML = "you're best attriutes are: " + zodiac[i].fortune
-						
+						document.getElementById("yourHoroscope").innerHTML =  zodiac[i].fortune
+						document.getElementById("element").innerHTML = "About Your Sign And Your Element: "
+						document.getElementById("elmInfo").innerHTML = zodiac[i].info
+						document.getElementById("elmName").innerHTML = zodiac[i].name
+						return
 				} else {
 						document.getElementById("yourSign").innerHTML = "thats not one of the signs, please try again!"
 						document.getElementById("yourHoroscope").innerHTML = ""
